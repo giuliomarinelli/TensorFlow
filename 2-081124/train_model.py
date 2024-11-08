c99a83f5da94561903784e54204538ea3bf7fd48
giuliomarinelli <giulio.marinelli@icloud.com>
2024-11-08 22:23:18 +0100

    Image trend analysis | imported dataset

--- 2-081124/train_model.py
+++ 2-081124/train_model.py
@@ -1,3 +1,6 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+fmnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = fmnist.load_data()
